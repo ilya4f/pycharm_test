153fa4e3e9884650cc12ee50f96cc1fe1dc3ee6c
ilya4f <ilya4f@mail.ru>
2023-03-17 22:51:22 +0300

    code editing_2

--- task_2.py
+++ task_2.py
@@ -1,5 +1,4 @@
-time_sek = int(input('Введите время в секундах:')) #Пользователь вводит время в секундах
+time_sek = int(input('Введите время в секундах:'))
 time_min = time_sek / 60
 time_hour = time_min / 60
-print('Время:', time_hour, ':' , time_min , ':' , time_sek) #Выводим время в формате ч:м:с
-
+print(f'Время: {time_hour} : {time_min} : {time_sek}')
