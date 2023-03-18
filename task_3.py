353522fd0b64264a9043c4d6a2eebbaebff25573
ilya4f <ilya4f@mail.ru>
2023-03-18 22:10:04 +0300

    code editing_2

--- task_3.py
+++ task_3.py
@@ -1,6 +1,5 @@
-n = int(input('Введите целое положительное число:')) #Пользователь вводит целое положительное число
-number = str(n)
-nn = number + number
-nnn = number + number + number
-sum = n + int(nn) + int(nnn)
-print('Сумма чисел равна:', sum) #Выводим сумму чисел n+nn+nnn
+a = int(input('Введите целое положительное число:'))
+n = int('%s' % a)
+nn = int('%s%s' % (a, a))
+nnn = int('%s%s%s' % (a, a, a))
+print(n + nn + nnn)
