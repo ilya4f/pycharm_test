d6f0005f2c973d991e29b6684f8db96bc158e906
ilya4f <ilya4f@mail.ru>
2023-03-22 11:45:52 +0300

    code editing_2

--- task_3.py
+++ task_3.py
@@ -1,25 +1,7 @@
-seasons_list = ['winter', 'spring', 'summer', 'autumn']
-month = int(input("Введите месяц по номеру:"))
-if month == 1 or month == 12 or month == 2:
-    print(seasons_list[0])
-elif month == 3 or month == 4 or month == 5:
-    print(seasons_list[1])
-elif month == 6 or month == 7 or month == 8:
-    print(seasons_list[2])
-elif month == 9 or month == 10 or month == 11:
-    print(seasons_list[3])
-else:
-    print("Такого месяца не существует")
+month = int(input('Введите номер месяца: '))
+my_list = ["Зима", "Зима", "Весна", "Весна", "Весна", "Лето", "Лето", "Лето", "Осень", "Осень", "Осень", "Зима"]
+print(f'Указанный номер месяца, соответствует времени года: {my_list[month -1]}')
 
-seasons_dict = {1: 'winter', 2: 'spring', 3: 'summer', 4: 'autumn'}
-month = int(input("Введите месяц по номеру:"))
-if month == 1 or month == 12 or month == 2:
-    print(seasons_dict.get(1))
-elif month == 3 or month == 4 or month == 5:
-    print(seasons_dict.get(2))
-elif month == 6 or month == 7 or month == 8:
-    print(seasons_dict.get(3))
-elif month == 9 or month == 10 or month == 11:
-    print(seasons_dict.get(4))
-else:
-    print("Такого месяца не существует")
+my_dict = {1: "Зима", 2: "Зима", 3: "Весна", 4: "Весна", 5: "Весна", 6: "Лето",
+           7: "Лето", 8: "Лето", 9: "Осень", 10: "Осень", 11: "Осень", 12: "Зима"}
+print(f'Указанный номер месяца, соответствует времени года: {my_dict.get(month)}')
