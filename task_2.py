3630e0216ba94f925047e58150f8522db5246ffb
ilya4f <ilya4f@mail.ru>
2023-03-22 11:21:47 +0300

    code editing_2

--- task_2.py
+++ task_2.py
@@ -1,8 +1,7 @@
-my_list = list(input('Введите целые числа через пробел:').split())
-new_list = []
-for i in range(0, len(my_list), 2):
-    next_i = i + 2
-    a = my_list[i:next_i]
-    a.reverse()
-    new_list.extend(a)
-print(new_list)
+my_list = input('Введите целые числа через пробел: ').split(' ')
+i, j = 0, 1
+while len(my_list) > j:
+    my_list[i], my_list[j] = my_list[j], my_list[i]
+    i += 2
+    j += 2
+print('Результат:', *my_list)
