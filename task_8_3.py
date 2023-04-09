cc56acd27dffedd4e1f47ab3902d0d8ee4c5d4f9
ilya4f <ilya4f@mail.ru>
2023-04-09 17:18:58 +0300

    code_1

---
+++ task_8_3.py
@@ -0,0 +1,35 @@
+'''Создайте собственный класс-исключение, который должен проверять содержимое списка на наличие только чисел.
+Проверить работу исключения на реальном примере. Необходимо запрашивать у пользователя данные и заполнять
+список только числами. Класс-исключение должен контролировать типы данных элементов списка.'''
+
+
+class NaNError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+def number_filter(string):
+    if string.isdigit():
+        return string
+    else:
+        try:
+            float(string)
+            return string
+        except ValueError:
+            raise NaNError(f'Error: {string} - is not a number')
+
+
+input_txt = ''
+counter = 1
+numbers_list = []
+print("Введите числа по одному, для выхода введите 'stop'")
+while input_txt != 'stop':
+    try:
+        input_txt = input(f"{counter}: ")
+        numbers_list.append(number_filter(input_txt))
+        counter += 1
+    except NaNError as e:
+        if input_txt != 'stop':
+            print(e.txt)
+
+print(f"Result list:\n{numbers_list}")
